7c463e094bd6239064dd5c6858c557d227a869f6
Michał Zieliński <michal@zielinscy.org.pl>
2017-07-17 17:24:35 +0000

    example modifications (squashed commits by Mikowhy95 and Hubert424)

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,32 +1,14 @@
 @NAME: basic
-@CATEGORY: Basic
 #include <hFramework.h>
 
-void hMain() {
-    // your code!
+void hMain() // main task containing your code
+{
+	LED1.on(); // turn on build-in LED #1 on PCB
+	
+	for(;;) // common loop for contious run!
+	{
+		LED1.toggle(); // switch state of LED1
+		sys.delay(500); // wait 500ms
+	}
 }
-#####
-@PORTS: stm32
-@BOARDS: robocore,core2,core2mini
-@NAME: basic_led
-@CATEGORY: Basic
-#include <hFramework.h>
-
-void hMain() {
-    while(true) {
-        LED1.toggle();
-        sys.delay(500);
-    }
-}
-#####
-@PORTS: stm32
-@NAME: basic_time
-@CATEGORY: Basic
-#include <hFramework.h>
-
-void hMain() {
-    while(true) {
-        printf("uptime %d", sys.getRefTime());
-        sys.delay(500);
-    }
-}
+ 
