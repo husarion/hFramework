08bf2f8741b9e5338e7dbc4bd8fd793a6df649f3
Hubert424 <hubert4242@gmail.com>
2017-08-07 17:40:28 +0200

    Update System_sys_mutex.cpp

--- examples/System_sys_mutex.cpp
+++ examples/System_sys_mutex.cpp
@@ -9,7 +9,7 @@ hMutex counter_mutex;
 
 void add_1()
 {
-	// Modify counter in mutex to ensure that the modification is not lost
+	// modify counter in mutex to ensure that the modification is not lost
 	counter_mutex.take();
 	int new_value = counter + 1;
 	counter = new_value;
