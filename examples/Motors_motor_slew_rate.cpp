57bc95482c540ae570d4b6ef91b812816cbc49eb
Hubert424 <hubert4242@gmail.com>
2017-08-07 17:33:44 +0200

    Update Motors_motor_slew_rate.cpp

--- examples/Motors_motor_slew_rate.cpp
+++ examples/Motors_motor_slew_rate.cpp
@@ -6,20 +6,20 @@
 
 void hMain()
 {
-	sys.taskCreate([]() // This creates a task that will control two motors
+	sys.taskCreate([]() // this creates a task that will control two motors
 				   {
-					   hMot2.setSlewRate(0.01); // Set motor2 rotation rise time to 10s
-					   for (;;)					// Task loop
+					   hMot2.setSlewRate(0.01); // set hMot2 rotation rise time to 10s
+					   for (;;)				
 					   {
-						   hMot1.setPower(400);  // Set motor1 power to 500
-						   hMot2.setPower(400);  // Set motor2 power to 500
-						   sys.delay(4000);		 // Sleep for 4000 miliseconds
-						   hMot1.setPower(-400); // Set motor power to 500 in reverse direction
-						   hMot2.setPower(-400); // Set motor power to 500 in reverse direction
-						   sys.delay(4000);		 // Sleep for 4000 miliseconds
+						   hMot1.setPower(400);  // set hMot1 power to 400
+						   hMot2.setPower(400);  // set hMot2 power to 400
+						   sys.delay(4000);		 
+						   hMot1.setPower(-400); // set motor power to 400 in reverse direction
+						   hMot2.setPower(-400); // set motor power to 400 in reverse direction
+						   sys.delay(4000);		 
 					   }
 				   });
 
-	sys.delay(10000);	 // Sleep for 10000 miliseconds
-	hMot2.setSlewRate(0); // Set motor2 rotation rise time to 0s
+	sys.delay(10000);	 
+	hMot2.setSlewRate(0); // set hMot2 rotation rise time to 0s
 }
