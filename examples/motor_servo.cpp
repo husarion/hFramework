3a03d095e8411840780db14555b782de394a15ac
Hubert424 <hubert4242@gmail.com>
2017-08-07 17:36:15 +0200

    Update motor_servo.cpp

--- examples/motor_servo.cpp
+++ examples/motor_servo.cpp
@@ -6,13 +6,13 @@
 
 void hMain()
 {
-	IServo &servo = hMot1.useAsServo();  // Enable usage of hMot1 as servo
-	servo.calibrate(-90, 700, 90, 1500); // Calibrate servo -> minAngle, minWidth, maxAngle, maxWidth
+	IServo &servo = hMot1.useAsServo();  // enable usage of hMot1 as servo
+	servo.calibrate(-90, 700, 90, 1500); // calibrate servo -> minAngle, minWidth, maxAngle, maxWidth
 
 	while (true) 
 	{
 		int time = sys.getRefTime();				
-		float pos = sinf(time / 3000.0f * 2 * M_PI); // Calculate successive sinusoidal values depend on time
-		servo.rotAbs(pos * 90.0f);					 // Rotate motor absolutely to set value
+		float pos = sinf(time / 3000.0f * 2 * M_PI); // calculate successive sinusoidal values depend on time
+		servo.rotAbs(pos * 90.0f);					 // rotate motor absolutely to set value
 	}
 }
