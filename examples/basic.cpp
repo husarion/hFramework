a7061559036fa1cc6930a688ff1411129a6f594f
radeknh <radeknh@users.noreply.github.com>
2017-08-16 08:46:15 +0200

    corrected typo and joined examples for CORE2 and RoboCORE

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,23 +1,5 @@
 @PORTS: stm32
-@BOARDS: core2,core2mini
-@NAME: basic
-@DESC: The "hello world" program
-@CATEGORY: Basics
-#include <hFramework.h>
-
-void hMain() // main function containing your code
-{
-	hLED1.on(); // turn on build-in LED1 on PCB
-	
-	for(;;) // common loop for contious run
-	{
-		hLED1.toggle(); // switch state of LED1
-		sys.delay(500); // wait 500ms
-	}
-}
- 
-@PORTS: stm32
-@BOARDS: robocore
+@BOARDS: core2,core2mini,robocore
 @NAME: basic
 @DESC: The "hello world" program
 @CATEGORY: Basics
@@ -25,9 +7,9 @@ void hMain() // main function containing your code
 
 void hMain() // main task containing your code
 {
-	hLED1.on(); // turn on build-in hLED #1 on PCB
+	hLED1.on(); // turn on build-in LED1 on PCB
 	
-	for(;;) // common loop for contious run!
+	for(;;) // common loop for continuous run
 	{
 		hLED1.toggle(); // switch state of hLED1
 		sys.delay(500); // wait 500ms
