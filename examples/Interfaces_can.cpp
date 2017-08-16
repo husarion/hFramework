d331e4b9d2bb1ac337105c5b41af4775df2667bd
radeknh <radeknh@users.noreply.github.com>
2017-08-16 08:42:06 +0200

    updated CAN example

--- examples/Interfaces_can.cpp
+++ examples/Interfaces_can.cpp
@@ -1,24 +1,5 @@
 @PORTS: stm32
 @BOARDS: core2,robocore
-@NAME: can_receive
-@DESC: The example shows how to receive frames with the CAN interface
-@CATEGORY: Interfaces
-#include <hFramework.h>
-
-void hMain()
-{
-	CAN.init();
-
-	CAN_frameRx frame;
-	bool frameReceived = CAN.waitFrame(frame, 100);
-	if (frameReceived)
-	{
-		// access the frame in `frame` variable
-	}
-}
-#####
-@PORTS: stm32
-@BOARDS: core2,robocore
 @NAME: can_send
 @DESC: The example shows how to transmit frames with the CAN interface
 @CATEGORY: Interfaces
@@ -33,8 +14,30 @@ void hMain()
 	tx.ExtId = 0;
 	tx.IDE = 0;
 	tx.RTR = 0;
-	tx.DLC = 2;
+	tx.DLC = 2; // Data Length Code (in bytes)
 	tx.data[0] = 0xab;
-	tx.data[1] = sys.getRefTime();
+	tx.data[1] = (int)sys.getRefTime();
 	CAN.sendFrame(tx);
+}
+// see more about CAN frame on Wikipedia: https://en.wikipedia.org/wiki/CAN_bus#Data_frame
+
+#####
+@PORTS: stm32
+@BOARDS: core2,robocore
+@NAME: can_receive
+@DESC: The example shows how to receive frames with the CAN interface
+@CATEGORY: Interfaces
+#include <hFramework.h>
+
+void hMain()
+{
+	CAN.init();
+
+	CAN_frameRx frame;
+	bool frameReceived = CAN.waitFrame(frame, 100);  // receive a frame with 100ms timeout
+	if (frameReceived)
+	{
+		// access the frame in `frame` variable
+		if(frame.data[0] == 0xab) printf("Received system time: %d\r\n", frame.data[1]); 
+	}
 }
