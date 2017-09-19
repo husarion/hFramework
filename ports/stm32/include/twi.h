a258efaa637cff92a47a52d2a03c436c4c73fed2
Mikołaj Żabiński <werewolf999999999@gmail.com>
2017-09-19 23:23:11 +0200

    Update twi.h

--- ports/stm32/include/twi.h
+++ ports/stm32/include/twi.h
@@ -17,35 +17,21 @@
 #ifndef twi_h
 #define twi_h
 
-  //#include <inttypes.h>
+  #ifndef TWI_FREQ
+  #define TWI_FREQ 100000
+  #endif
 
-  //#define ATMEGA8
-
-  //#ifndef TWI_FREQ
-  //#define TWI_FREQ 100000L
-  //#endif
-
-  //#ifndef TWI_BUFFER_LENGTH
-  //#define TWI_BUFFER_LENGTH 32
-  //#endif
-
-  //#define TWI_READY 0
-  //#define TWI_MRX   1
-  //#define TWI_MTX   2
-  //#define TWI_SRX   3
-  //#define TWI_STX   4
-  
-  //void twi_init(void);
-  //void twi_disable(void);
-  //void twi_setAddress(uint8_t);
-  //void twi_setFrequency(uint32_t);
-  //uint8_t twi_readFrom(uint8_t, uint8_t*, uint8_t, uint8_t);
-  //uint8_t twi_writeTo(uint8_t, uint8_t*, uint8_t, uint8_t, uint8_t);
-  //uint8_t twi_transmit(const uint8_t*, uint8_t);
-  //void twi_attachSlaveRxEvent( void (*)(uint8_t*, int) );
-  //void twi_attachSlaveTxEvent( void (*)(void) );
-  //void twi_reply(uint8_t);
-  //void twi_stop(void);
-  //void twi_releaseBus(void);
+  void twi_init(void);
+  void twi_disable(void);
+  void twi_setAddress(uint8_t);
+  void twi_setFrequency(uint32_t);
+  uint8_t twi_readFrom(uint8_t, uint8_t*, uint8_t, uint8_t);
+  uint8_t twi_writeTo(uint8_t, uint8_t*, uint8_t, uint8_t, uint8_t);
+  uint8_t twi_transmit(const uint8_t*, uint8_t);
+  void twi_attachSlaveRxEvent( void (*)(uint8_t*, int) );
+  void twi_attachSlaveTxEvent( void (*)(void) );
+  void twi_reply(uint8_t);
+  void twi_stop(void);
+  void twi_releaseBus(void);
 
 #endif
