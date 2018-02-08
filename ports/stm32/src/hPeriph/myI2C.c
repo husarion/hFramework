f28aca4f57ced32a2594a12f3f66d8a9bf3045e0
Dominik <dominik.nwk@gmail.com>
2018-02-08 18:11:59 +0100

    modified delay in I2C rw function

--- ports/stm32/src/hPeriph/myI2C.c
+++ ports/stm32/src/hPeriph/myI2C.c
@@ -260,7 +260,7 @@ I2C_ERROR myI2C_read(uint8_t nr, uint8_t addr, uint8_t* dataBuf, uint32_t bufSiz
 		return I2C_ERROR_TIMEOUT;
 	}
 }
-I2C_ERROR myI2C_read_write(uint8_t nr, uint8_t addr, uint8_t* dataBuf_TX, uint32_t bufSize_TX, uint8_t* dataBuf_RX, uint32_t bufSize_RX)
+I2C_ERROR myI2C_read_write(uint8_t nr, uint8_t addr, uint8_t* dataBuf_TX, uint32_t bufSize_TX, uint8_t* dataBuf_RX, uint32_t bufSize_RX, uin32_t rxDelay)
 {
 	I2C_config_t* i2c = &i2cs[nr];
 	I2C_ERROR result;
@@ -286,7 +286,7 @@ I2C_ERROR myI2C_read_write(uint8_t nr, uint8_t addr, uint8_t* dataBuf_TX, uint32
 		GPIO_Init(i2c->sda.port, &GPIO_InitStructure);
 
 		GPIO_ResetBits(i2c->scl.port, i2c->scl.pin);
-		sys_delay_us(500);
+		sys_delay_us(rxDelay);
 		GPIO_SetBits(i2c->scl.port, i2c->scl.pin);
 
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
